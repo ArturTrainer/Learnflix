d41c7e533a944f58c0120837c3b6a5c630106abb
Artur Akhmetshin <artur.akhmetshin@outlook.com>
2020-10-19 16:17:49 +0300

    Initial Commit

---
+++ Learnflix/LearnflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnflixApp.swift
+//  Learnflix
+//
+//  Created by Artur Akhmetshin on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct LearnflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
